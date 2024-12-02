4fe4edc8e46318aad5238d861f314b4595bf9567
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-12-01 22:26:15 -0800

    fix base 2048-dec2024

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2048-nov-29-2024/',  // Replace with your repository name
+  base: '/2048-dec2024/',  // Replace with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
